468186759de9d405cb686b5d554851435708554c
Md Roni Ahamed <111082653+MdRoniAhamed@users.noreply.github.com>
2022-08-14 17:25:39 +0600

    Update Problem no 2.cpp

--- Problem no 2.cpp
+++ Problem no 2.cpp
@@ -8,7 +8,7 @@ int main()
     if(n>=3) cout<<3<<" ";
     if(n>=5) cout<<5<<" ";
     if(n>=7) cout<<7<<" ";
-    for(int i=2; i<n; i++)
+    for(int i=2; i<=n; i++)
     {
         if(i%2==0||i%3==0||i%5==0||i%7==0){
 
